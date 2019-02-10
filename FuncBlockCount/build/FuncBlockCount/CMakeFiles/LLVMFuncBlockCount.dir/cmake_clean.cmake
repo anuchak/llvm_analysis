b11d8ccfd7a1a20c56d7bb5e621f6cf128e8f0b9
Anurag Chakraborty <anurag.chakraborty98@gmail.com>
2019-02-10 20:58:30 +0530

    Add files via upload

---
+++ FuncBlockCount/build/FuncBlockCount/CMakeFiles/LLVMFuncBlockCount.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LLVMFuncBlockCount.dir/FuncBlockCount.o"
+  "libLLVMFuncBlockCount.pdb"
+  "libLLVMFuncBlockCount.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LLVMFuncBlockCount.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
